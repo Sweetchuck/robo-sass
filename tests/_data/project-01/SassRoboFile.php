e1f8a2ec73da69d98c827af7cd160c368dc68e42
Andor Dávid <andor.david@cheppers.com>
2017-10-21 14:17:00 +0200

    Chenge the data type from int to string in \SassRoboFile::compileFiles
    https://github.com/consolidation/annotated-command\#option-default-values

--- tests/_data/project-01/SassRoboFile.php
+++ tests/_data/project-01/SassRoboFile.php
@@ -17,19 +17,11 @@ class SassRoboFile extends Tasks
             'cssPath' => '',
             'mapPath' => '',
             'embed' => false,
-            'indent' => 4,
-            'precision' => 4,
+            'indent' => '4',
+            'precision' => '4',
             'includePaths' => [],
         ]
     ): TaskInterface {
-        // There is bug with the default values if the data type is numeric.
-        // Default values are overwritten with NULL values.
-        foreach (['indent' => 4, 'precision' => 4] as $key => $value) {
-            if (!isset($options[$key])) {
-                $options[$key] = $value;
-            }
-        }
-
         $files = (new Finder())
             ->in(__DIR__ . '/scss')
             ->files()
